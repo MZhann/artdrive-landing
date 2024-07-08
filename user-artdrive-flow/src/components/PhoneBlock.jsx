14e28116a61cacf12f117c53e42cc8d63b549cd4
MZhann <zhanbolat.mukan2004@gmail.com>
2024-07-08 07:36:30 +0500

    fix: fix of scroll text

--- user-artdrive-flow/src/components/PhoneBlock.jsx
+++ user-artdrive-flow/src/components/PhoneBlock.jsx
@@ -7,15 +7,16 @@ const PhoneBlock = () => {
 
     const handleScroll = () => {
         const sections = document.querySelectorAll(".scroll-section");
-        const scrollPosition = window.scrollY + window.innerHeight / 1.85;
+        const scrollPosition = window.scrollY + window.innerHeight / 7;
 
         sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop;
+            const sectionTop = section.getBoundingClientRect().top + window.scrollY;
             const sectionHeight = section.clientHeight;
+            const buffer = window.innerHeight / 4; // Add buffer to the range
 
             if (
-                scrollPosition >= sectionTop &&
-                scrollPosition < sectionTop + sectionHeight
+                scrollPosition >= sectionTop - buffer &&
+                scrollPosition < sectionTop + sectionHeight + buffer
             ) {
                 setActiveIndex(index);
             }
@@ -42,55 +43,49 @@ const PhoneBlock = () => {
     };
 
     return (
-        <div className="flex flex-col bg-black items-start pt-10 lg:pt-20 pb-4 mb-[-5px] px-4 sm:px-20 lg:px-[5%] lg:items-center">
-            <div className="flex justify-center w-full lg:w-[90%] xl:w-[1100px]">
+        <div className="flex flex-col bg-black justify-start items-start pt-10 lg:pt-20 pb-4 mb-[-5px] px-4 sm:px-20 lg:px-[5%] lg:items-center">
+            <div className="flex justify-center items-center w-full  lg:w-[90%] xl:w-[1100px]">
                 <div className="flex flex-col items-center justify-center md:flex-row md:space-x-10 w-[90%]">
                     <div className="text-center md:hidden scroll-section text-4xl mb-10 text-white">
-                        {/* <div className={`scroll-section mb-5 ${textStyles(0)}`}> */}
                         Convert{" "}
                         <span className="text-gradient font-bold">Likes</span>{" "}
                         into <br className="md:hidden"></br>Dollars $
-                        {/* Convert <span className={gradientTextStyles(0)}>Likes</span> into Dollars $ */}
                     </div>
                     <div className="md:w-[440px]">
                         <Image
                             src={phone}
-                            width={300}
-                            height={600}
                             alt="screenshot of an app"
-                            className="xl:h-[580px] h-[450px] md:h-[380px] lg:h-[450px] w-auto"
+                            width={1000}
+                            height={1500}
+                            className="h-[350px] md:h-[380px] lg:h-[450px] xl:h-[580px] w-auto "
                         />
-                       
                     </div>
-                    <div className="flex flex-col justify-center text-[1.2rem] lg:text-2xl xl:text-3xl leading-relaxed lg:leading-relaxed xl:leading-relaxed max-w-[400px] sm:max-w-[400px] md:max-w-[550px] md:text-[1.3rem] text-center md:text-left mt-10 font-bold font-montserrat  tracking-tight leading-relaxed">
-                        <div className="hidden md:block scroll-section text-4xl md:text-2xl lg:text-3xl xl:text-4xl  mb-5 text-white">
-                            {/* <div className={`scroll-section mb-5 ${textStyles(0)}`}> */}
+                    <div className="flex flex-col justify-start text-[1.2rem] lg:text-2xl xl:text-3xl px-4 md:ml-10 lg:leading-relaxed xl:leading-relaxed max-w-[400px] sm:max-w-[400px] md:max-w-[550px] md:text-[1.3rem] text-center md:text-left font-bold font-montserrat tracking-tight leading-relaxed">
+                        <div className="hidden md:block text-4xl md:text-2xl lg:text-3xl xl:text-4xl lg:mb-8 text-white">
                             Convert <span className="text-gradient">Likes</span>{" "}
                             into Dollars $
-                            {/* Convert <span className={gradientTextStyles(0)}>Likes</span> into Dollars $ */}
                         </div>
-                        <div className={`scroll-section ${textStyles(1)}`}>
-                            <span className={gradientTextStyles(1)}>
+                        <div className={`scroll-section mt-10 ${textStyles(0)}`}>
+                            <span className={gradientTextStyles(0)}>
                                 Get noticed Fast.
                             </span>{" "}
-                            Share your <br className="hidden" /> content and
+                            Share your <br className="hidden md:block" /> content and
                             gain instant exposure.
                         </div>
-                        <div className={`scroll-section ${textStyles(2)}`}>
+                        <div className={`scroll-section ${textStyles(1)}`}>
                             Earn what you Deserve.{" "}
-                            <span className={gradientTextStyles(2)}>
+                            <span className={gradientTextStyles(1)}>
                                 Monetize
                             </span>{" "}
-                            <br className="hidden" /> your art in{" "}
-                            <span className={gradientTextStyles(2)}>
+                            <br className="hidden md:block" /> your art in{" "}
+                            <span className={gradientTextStyles(1)}>
                                 under 2 Hours
                             </span>
                             .
                         </div>
-                        <div className={`scroll-section ${textStyles(3)}`}>
-                            Stay ahead of the Game. Quickly find,{" "}
-                            <br className="hidden" /> test and{" "}
-                            <span className={gradientTextStyles(3)}>
+                        <div className={`scroll-section ${textStyles(2)}`}>
+                            Stay ahead of the Game. Quickly find, test and{" "}
+                            <span className={gradientTextStyles(2)}>
                                 validate your content Ideas.
                             </span>
                         </div>
