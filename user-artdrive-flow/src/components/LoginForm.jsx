7b490a7c2700f239b21c73cca68e9920882ba7df
MZhann <zhanbolat.mukan2004@gmail.com>
2024-07-26 09:39:38 +0500

    feat: google login

--- user-artdrive-flow/src/components/LoginForm.jsx
+++ user-artdrive-flow/src/components/LoginForm.jsx
@@ -1,3 +1,101 @@
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import { signIn, useSession } from "next-auth/react";
+// import Image from "next/image";
+// import googleLogo from "../../public/images/google.svg";
+// import jwt from "jsonwebtoken";
+
+// const LoginForm = ({ language }) => {
+//     const router = useRouter();
+//     const { data: session, status } = useSession();
+
+//     console.log("in login form");
+//     console.log("Session status:", status);
+
+//     useEffect(() => {
+
+//         if (status === "authenticated" && session) {
+//             // console.log("in if session");
+
+//             const { user } = session;
+//             const SECRET_KEY = process.env.SECRET_KEY || "hardcoded_secret_key";
+
+//             if (SECRET_KEY) {
+//                 // console.log("in if secretKey");
+
+//                 try {
+//                     const token = jwt.sign(
+//                         {
+//                             email: user.email,
+//                             name: user.name,
+//                             image: user.image,
+//                         },
+//                         SECRET_KEY,
+//                         { expiresIn: "1h" }
+//                     );
+
+//                     console.log("JWT token generated");
+//                     sendTokenToBackend(token);
+//                     // localStorage.setItem('accessToken', token)
+//                 } catch (error) {
+//                     console.error("Error generating token:", error);
+//                 }
+//             } else {
+//                 console.error("SECRET_KEY is not defined");
+//             }
+//         }
+//     }, [session, status]);
+
+//     const sendTokenToBackend = async (idToken) => {
+//         try {
+//             const response = await fetch(
+//                 "https://artdrivebackend-production.up.railway.app/api/v1/google-login/",
+//                 {
+//                     method: "POST",
+//                     headers: {
+//                         "Content-Type": "application/json",
+//                     },
+//                     body: JSON.stringify({ id_token: idToken }),
+//                 }
+//             );
+
+//             if (response.ok) {
+//                 const data = await response.json();
+//                 // console.log('data: ', data);
+//                 // console.log('response: ', response);
+//                 const { jwt: receivedJwt } = data;
+//                 // console.log("Received JWT from backend:", receivedJwt);
+//                 // Store the JWT or perform any further actions required
+//                 router.push("/auth/congrats");
+//             } else {
+//                 console.error("Failed to send user info to backend");
+//             }
+//         } catch (error) {
+//             console.error("An error occurred:", error);
+//         }
+//     };
+
+//     return (
+//         <button
+//             onClick={() => signIn("google")}
+//             className="w-full flex items-center text-white text-xs h-[50px] justify-center space-x-4 mt-4 p-5 bg-[#212022] rounded-2xl border-2 border-[#737373] "
+//         >
+//             <Image
+//                 src={googleLogo}
+//                 width={30}
+//                 height={30}
+//                 alt="google logo"
+//                 className="mr-2"
+//             />
+//             {language === "en"
+//                 ? "Sign up with Google"
+//                 : "Зарегистрироваться с Google"}
+//         </button>
+//     );
+// };
+
+// export default LoginForm;
+
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { signIn, useSession } from "next-auth/react";
@@ -13,16 +111,11 @@ const LoginForm = ({ language }) => {
     console.log("Session status:", status);
 
     useEffect(() => {
-
         if (status === "authenticated" && session) {
-            // console.log("in if session");
-
             const { user } = session;
             const SECRET_KEY = process.env.SECRET_KEY || "hardcoded_secret_key";
 
             if (SECRET_KEY) {
-                // console.log("in if secretKey");
-
                 try {
                     const token = jwt.sign(
                         {
@@ -60,12 +153,14 @@ const LoginForm = ({ language }) => {
 
             if (response.ok) {
                 const data = await response.json();
-                // console.log('data: ', data);
-                // console.log('response: ', response);
-                const { jwt: receivedJwt } = data;
-                // console.log("Received JWT from backend:", receivedJwt);
-                // Store the JWT or perform any further actions required
-                router.push("/auth/congrats");
+                const { access, refresh } = data;
+
+                // Save tokens to localStorage
+                localStorage.setItem('accessToken', access);
+                localStorage.setItem('refreshToken', refresh);
+
+                console.log("Tokens saved to localStorage");
+                router.push("/auth/congrats-google");
             } else {
                 console.error("Failed to send user info to backend");
             }
---
+++ user-artdrive-flow/src/pages/auth/congrats-google/index.jsx
@@ -0,0 +1,98 @@
+import "../../../app/globals.css";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+
+import xLogo from "../../../../public/images/x.svg"; 
+import discordLogo from "../../../../public/images/discord.svg";
+import {signOut, useSession} from "next-auth/react";
+
+const Congrats = () => {
+    const router = useRouter();
+    const { name } = router.query;
+    const [userName, setUserName] = useState("");
+    const [language, setLanguage] = useState("en");
+    const {data: session} = useSession()
+    const back = () => {
+        router.push("/");
+    };
+
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem("language");
+        if (storedLanguage) {
+            setLanguage(storedLanguage);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (name) {
+            setUserName(name);
+        }
+        if (session) {
+            setUserName(session.user?.name);
+        }
+    }, [name, session]);
+
+    return (
+        <div className="w-full h-[100vh] flex items-center justify-center dark-purple-gradient bg-cover">
+            <div className="w-[500px] mx-3 flex flex-col items-center font-montserrat p-6 mt-[-40px] rounded-3xl">
+                <h1 className="text-xl mb-10 uppercase font-semibold mt-[-150px] text-white font-montserrat text-center">
+                    {language == "en" ? "Congratulations," : "Поздравляем,"}{" "}
+                    <br></br>
+                    {userName}!
+                </h1>
+                <div className="w-full flex flex-col items-center">
+                    <p className="text-sm mb-2 text-[#DADADA] font-montserrat text-center flex justify-center w-[230px] text-nowrap">
+                        {language == "en"
+                            ? "You are now part of ArtDrive beta."
+                            : "Теперь вы часть ArtDrive beta."}{" "}
+                    </p>
+                    <p className="text-sm mb-2 text-[#DADADA] font-montserrat text-center flex justify-center w-[220px]">
+                        {language == "en"
+                            ? "Subscribe to follow updates:"
+                            : "Подпишитесь, чтобы следить за обновлениями"}{" "}
+                    </p>
+                </div>
+
+                <div className="flex space-x-5 mt-5 items-center">
+                    <Link href={'https://x.com/ArtDrive_io'}>
+                        <Image
+                            src={xLogo}
+                            width={300}
+                            height={300}
+                            className="w-8 h-8"
+                            alt="x/twitter logo"
+                        />
+                    </Link>
+                    <Link href={'https://discord.gg/byq8huwJ'}>
+                        <Image
+                            src={discordLogo}
+                            width={300}
+                            height={300}
+                            className="w-9 h-9"
+                            alt="discord logo"
+                        />
+                    </Link>
+                </div>
+
+                <button
+                    className="w-full p-5 mt-10 h-[50px] flex items-center justify-center text-white text-lg rounded-xl purple-gradient"
+                    onClick={() => {
+                        signOut().then(() => {
+                            console.log("Start")
+                            window.location.href = "/"
+                        })
+                    }}
+                >
+                    {language == "en"
+                        ? "Start"
+                        : "старт"}
+                </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Congrats;
